64f6f615f1cc0847370e58497b5e8e417a74d782
Artem Tarasov <artem@dintecom.com>
2021-09-01 08:32:48 -0400

    Improved validation for native provider

--- projects/picker/src/lib/date-time/adapter/moment/moment-date-time-adapter.class.ts
+++ projects/picker/src/lib/date-time/adapter/moment/moment-date-time-adapter.class.ts
@@ -174,6 +174,10 @@ export class MomentDateTimeAdapter extends DateTimeAdapter<Moment> {
         return this.clone(date).isValid();
     }
 
+    public isValidFormat(value: any, parseFormat: any): boolean {
+        return moment(value, parseFormat, true).isValid();
+    }
+
     public invalid(): Moment {
         return moment.invalid();
     }
