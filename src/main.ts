3772419fe181a8c78f7b5871cfd800e505f8a344
Tobias Siegmund <tobias.siegmund@metals-hub.com>
2025-12-18 12:11:45 +0100

    Angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { OWL_DATE_TIME_LOCALE, OptionsTokens } from '../projects/picker/src/public_api';
@@ -5,7 +6,7 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideAnimations(),
+    provideZoneChangeDetection(),provideAnimations(),
     {
       provide: OWL_DATE_TIME_LOCALE,
       useValue: 'en-US'
